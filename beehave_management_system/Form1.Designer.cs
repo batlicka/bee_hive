044eb180d0a73528d30954a217e13c7cb04a71d8
batlicka <46314552+batlicka@users.noreply.github.com>
2019-01-30 14:22:25 +0100

    elementary_commit

---
+++ beehave_management_system/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace beehave_management_system
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.workerBeeJob = new System.Windows.Forms.ComboBox();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.nextShift = new System.Windows.Forms.Button();
+            this.report = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.assignJob = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "worker bee assigement";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "job";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(326, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "shifts";
+            // 
+            // workerBeeJob
+            // 
+            this.workerBeeJob.FormattingEnabled = true;
+            this.workerBeeJob.Items.AddRange(new object[] {
+            "Nectar collector",
+            "Honey manufacturing",
+            "Egg care",
+            "Baby bee tutoring",
+            "Hive maintenance",
+            "Sting patrol"});
+            this.workerBeeJob.Location = new System.Drawing.Point(27, 93);
+            this.workerBeeJob.Name = "workerBeeJob";
+            this.workerBeeJob.Size = new System.Drawing.Size(225, 24);
+            this.workerBeeJob.TabIndex = 3;
+            // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(329, 94);
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(120, 22);
+            this.shifts.TabIndex = 4;
+            // 
+            // nextShift
+            // 
+            this.nextShift.Location = new System.Drawing.Point(501, 26);
+            this.nextShift.Name = "nextShift";
+            this.nextShift.Size = new System.Drawing.Size(225, 90);
+            this.nextShift.TabIndex = 5;
+            this.nextShift.Text = "work the next shift";
+            this.nextShift.UseVisualStyleBackColor = true;
+            this.nextShift.Click += new System.EventHandler(this.nextShift_Click);
+            // 
+            // report
+            // 
+            this.report.Location = new System.Drawing.Point(27, 160);
+            this.report.Name = "report";
+            this.report.Size = new System.Drawing.Size(711, 310);
+            this.report.TabIndex = 6;
+            this.report.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(781, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // assignJob
+            // 
+            this.assignJob.Location = new System.Drawing.Point(27, 131);
+            this.assignJob.Name = "assignJob";
+            this.assignJob.Size = new System.Drawing.Size(340, 23);
+            this.assignJob.TabIndex = 8;
+            this.assignJob.Text = "assign this job to a bee";
+            this.assignJob.UseVisualStyleBackColor = true;
+            this.assignJob.Click += new System.EventHandler(this.assignJob_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 499);
+            this.Controls.Add(this.assignJob);
+            this.Controls.Add(this.report);
+            this.Controls.Add(this.nextShift);
+            this.Controls.Add(this.shifts);
+            this.Controls.Add(this.workerBeeJob);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox workerBeeJob;
+        private System.Windows.Forms.NumericUpDown shifts;
+        private System.Windows.Forms.Button nextShift;
+        private System.Windows.Forms.RichTextBox report;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button assignJob;
+    }
+}
+
---
+++ beehave_management_system/Worker.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beehave_management_system
+{
+    class Worker : Bee
+    {
+        public Worker(string[] jobsICanDo, double weightMg) : base(weightMg)
+        {
+            this.jobsICanDo = jobsICanDo;
+        }
+        public int ShiftsLeft
+        {
+            get
+            {
+                return shiftsToWork - shiftsWorked;
+            }
+        }        
+        public string CurrentJob
+        {
+            get
+            {
+                return currentJob;
+            }
+        }
+        private string[] jobsICanDo;
+        private int shiftsToWork;
+        private int shiftsWorked;
+        private string currentJob = "";
+        public const double honeyUnitsPerShiftWorkedg = 1;
+        public bool DoThisJob(string job, int numberOfShifts)
+        {   //If that worker is not already doing the job
+            if (!String.IsNullOrEmpty(currentJob))
+                return false;
+            for (int i = 0; i < jobsICanDo.Length; i++)
+                if (jobsICanDo[i] == job)
+                {
+                    currentJob = job;
+                    this.shiftsToWork = numberOfShifts;
+                    shiftsWorked = 0;
+                    return true;
+                }
+            return false;
+        }
+        public bool DidYouFinish()
+        {
+            if (String.IsNullOrEmpty(currentJob))//pokud current job nic neobsahuje, metoda vrátí true
+                return false;
+            shiftsWorked++;
+            if (shiftsWorked > shiftsToWork)
+            {
+                shiftsWorked = 0;
+                shiftsToWork = 0;
+                currentJob = "";
+                return true;
+            }
+            else
+                return false;
+        }
+        public override double HoneyConsumptionRate()
+        {
+            return base.HoneyConsumptionRate()+ (shiftsWorked* honeyUnitsPerShiftWorkedg);
+        }
+    }
+}
